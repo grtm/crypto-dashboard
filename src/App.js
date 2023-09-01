029898bf7cb2e7ff7934581c5abb24e15a2f6d19
DannyUzo <140250311+DannyUzo@users.noreply.github.com>
2023-09-01 10:58:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,11 @@ function App() {
       <div className="App" id={theme}>
         <div className="body">
           
-
+          {shouldRenderSidenav && <>
+            <div className="side">
+              {shouldRenderSidenav && <Sidenav />}
+            </div>
+          </>}
             {shouldRenderSidenavHome && <>
             <div className="side">
               {shouldRenderSidenavHome && <Sidenav />}
